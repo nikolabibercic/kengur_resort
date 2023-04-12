591f22bbf3edf4551f79634cdccdeea9bcb81741
nikolabibercic <nikolabibercic@gmail.com>
2023-04-12 17:39:30 +0200

    home page - header,main content i google map

---
+++ main.js
@@ -0,0 +1,22 @@
+var header = document.getElementById('header');
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var btn4 = document.getElementById('btn4');
+var btn5 = document.getElementById('btn5');
+
+btn1.addEventListener('click',() => {
+    header.style.backgroundImage = "url('img/header1.jpg')";
+});
+btn2.addEventListener('click',() => {
+    header.style.backgroundImage = "url('img/header2.jpg')";
+});
+btn3.addEventListener('click',() => {
+    header.style.backgroundImage = "url('img/header3.jpg')";
+});
+btn4.addEventListener('click',() => {
+    header.style.backgroundImage = "url('img/header4.jpg')";
+});
+btn5.addEventListener('click',() => {
+    header.style.backgroundImage = "url('img/header5.jpg')";
+});
